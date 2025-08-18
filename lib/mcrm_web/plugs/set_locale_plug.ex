07ffbb24067da2146b8e5d9d5b5eaab701f6dd0a
doryan.dahon <doryan.dahon@mydiapason.com>
2025-08-18 23:34:40 +0200

    add locale switcher

--- lib/mcrm_web/plugs/set_locale_plug.ex
+++ lib/mcrm_web/plugs/set_locale_plug.ex
@@ -5,10 +5,22 @@ defmodule McrmWeb.Plugs.SetLocale do
 
   def init(_options), do: nil # 3
 
-  def call(%Plug.Conn{params: %{"locale" => locale}} = conn, _options) when locale in @supported_locales do #  4
-    McrmWeb.Gettext |> Gettext.put_locale(locale)
-    conn
+  @spec call(any(), any()) :: any()
+  def call(conn, _options) do
+    case fetch_locale_from(conn) do
+      nil -> conn
+      locale ->
+        McrmWeb.Gettext |> Gettext.put_locale(locale)
+        conn |> put_resp_cookie "locale", locale, max_age: 365*24*60*60
+    end
   end
 
-  def call(conn, _options), do: conn # 5
+  defp fetch_locale_from(conn) do
+    (conn.params["locale"] || conn.cookies["locale"]) |>
+    check_locale
+  end
+
+  defp check_locale(locale) when locale in @supported_locales, do: locale
+
+  defp check_locale(_), do: nil
 end
